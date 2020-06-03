e10c7bed40222d12bf4eb65ad790e8d84c5b3755
EC2 Default User <ec2-user@ip-172-31-5-11.us-east-2.compute.internal>
2020-06-03 17:08:54 +0000

    added loader.io

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   password: 'postgres',
-  host: 'localhost',
+  host: '18.216.50.227',
   port: 5432,
   database: 'postgres',
   max: 10,
